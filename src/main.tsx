2598be00f13a1d2afe6e851953d9b5b98d9089db
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-12-02 09:48:42 +0530

    Update build configuration and dependencies; enhance TypeScript settings and add Leaflet CSS import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import 'leaflet/dist/leaflet.css';
+
 
 import WebApp from '@twa-dev/sdk'
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
-  build: {
-    outDir: './dist'
-  },
-  base: '/vite-boilerplate/'
-});
+  plugins: [react()],
+})
